2a5b21ee307f06df7a03866e62d14f873ea3bf6c
stefanedelman <150808948+stefanedelman@users.noreply.github.com>
2025-11-27 01:55:53 +0100

    better arrow

--- src/App.vue
+++ src/App.vue
@@ -75,7 +75,9 @@ onMounted(() => {
     <Footer />
 
     <button class="scroll-to-top" :class="{ 'visible': showScrollTop }" @click="scrollToTop" aria-label="Scroll to top">
-        ↑
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+            <path d="M12 19V5M5 12l7-7 7 7"/>
+        </svg>
     </button>
 </template>
 
